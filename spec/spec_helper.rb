6754a008f1de8df9fd2976b5b312b1ba2410c5d4
Tyler Hunt <tyler@tylerhunt.com>
2011-05-07 14:23:19 -0400

    Added additional RSpec configuration.
    Set the default mocking library and set up support for focus.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,3 +9,9 @@ require 'remockable'
 Dir[File.expand_path('../support/**/*.rb', __FILE__)].each do |file|
   require(file)
 end
+
+RSpec.configure do |config|
+  config.mock_with :rspec
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
+end
