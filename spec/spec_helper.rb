56c56cdb9f9234d634ab0dc599452c08c625a7a3
Tyler Hunt <tyler@tylerhunt.com>
2017-01-16 11:42:48 -0500

    Update RSpec configuration for RSpec 3.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,12 +4,27 @@ require 'active_record'
 require 'remockable'
 
 # Require supporting files with custom matchers and macros, etc.
-Dir[File.expand_path('../support/**/*.rb', __FILE__)].each do |file|
-  require(file)
-end
+Dir[File.expand_path('support/**/*.rb', __dir__)].each { |file| require file }
 
 RSpec.configure do |config|
-  config.mock_with :rspec
-  config.filter_run focus: true
+  config.filter_run :focus
   config.run_all_when_everything_filtered = true
+  config.example_status_persistence_file_path = 'spec/examples.txt'
+
+  if config.files_to_run.one?
+    config.default_formatter = 'doc'
+  end
+
+  config.order = :random
+  Kernel.srand config.seed
+
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
+    mocks.verify_partial_doubles = true
+  end
 end
